c13efdf6ae8086780e894f1ff501e55fd30e1fc1
mamikonyanhovik <58630430+mamikonyanhovik@users.noreply.github.com>
2019-12-14 13:13:03 +0400

    Add files via upload

---
+++ h_2,ex_9.js
@@ -0,0 +1,33 @@
+var n = +prompt ();
+
+var i = 0;
+var j = 0;
+
+if (n%2===0 ) {
+  if (!Math.floor(n/10)) {
+    i +=1;
+  }
+}
+
+id (n%3 === 0) {
+  if (n%10 === 1) {
+    j +=1;
+  }
+}
+
+
+
+// answer
+
+var n = +prompt ();
+
+var i = 0;
+var j = 0;
+
+if (n%2===0 && !Math.floor(n/10)) {
+      i +=1;
+  }
+
+id (n%3 === 0 && n%10 === 1) {
+    j +=1;
+  }
